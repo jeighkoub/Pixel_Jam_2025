8fbcfd3cc9c1c6001bbb56bb4fd7ad8da1e4037a
jeighkoub <92068755+jeighkoub@users.noreply.github.com>
2025-05-14 16:33:05 -0400

    waving tower

---
+++ tower.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var scoops: Array
+var time: float = 0.0
+var tilt_speed = PI/4     # radians per second
+var max_angle = PI / 4  # maximum lean angle (45 degrees)
+var SCOOP_RADIUS = 50
+var L = 2 * SCOOP_RADIUS
+
+func _ready():
+	for child in get_children():
+		scoops.append(child)
+
+func _process(delta: float) -> void:
+	time += delta
+	var theta = max_angle * sin(time * tilt_speed)
+	
+	for i in range(scoops.size()):
+		var x: float = 0
+		var y: float = 0
+		for k in range(i):
+			x += L * sin(theta*k)
+			y += -1 * L * cos(theta*k)
+		scoops[i].position = Vector2(x, y)
