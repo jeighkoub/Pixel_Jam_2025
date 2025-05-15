dea9f036b372b007cd43d5d19564f72aa7e83089
DIGIT-DILL <charleshowarddill@gmail.com>
2025-05-15 00:10:42 -0400

    Edits N Stuff

---
+++ middle_cutscene.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var color_rect = $CanvasLayer/ColorRect  
+
+var tween: Tween
+
+func _ready():
+	if color_rect:
+		color_rect.size = Vector2(320, 180)
+		color_rect.position = Vector2(0, 0)
+		color_rect.color = Color(0, 0, 0, 1)
+		color_rect.z_index = 100 
+		color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	start_cutscene()
+
+func start_cutscene():
+	if color_rect:
+		tween = create_tween()
+		tween.tween_property(color_rect, "color", Color(0, 0, 0, 0), 1.0).set_trans(Tween.TRANS_LINEAR)
+		await tween.finished
+
+		await get_tree().create_timer(2.0).timeout
+
+		tween = create_tween()
+		tween.tween_property(color_rect, "color", Color(0, 0, 0, 1.0), 1.0).set_trans(Tween.TRANS_LINEAR)
+		await tween.finished
+
+		get_tree().change_scene_to_file("res://main.tscn")
