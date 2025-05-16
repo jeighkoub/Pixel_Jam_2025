885db00d85f7c0c8e991c06ed05079950e4e7192
jeighkoub <92068755+jeighkoub@users.noreply.github.com>
2025-05-15 21:38:59 -0400

    kfsjngjkfsn

--- dropper.gd
+++ dropper.gd
@@ -6,16 +6,20 @@ extends Node2D
 @export var green: Resource
 @export var red: Resource
 
+func _ready():
+	create_scoop("red")
 
 func create_scoop(color: String) -> void:
 	scoop.global_position = self.global_position
 	match color:
-		"blue":
+		"blue", "b":
 			scoop.get_node("Sprite2D").texture = blue
-		"green":
+		"green", "g":
 			scoop.get_node("Sprite2D").texture = green
-		"red":
+		"red", "r":
 			scoop.get_node("Sprite2D").texture = red
+	scoop.visible = true
+	
 
 func drop():
 	scoop.velocity = Vector2(0,100)
