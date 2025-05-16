4681ff69c7b77b8a1876518c27ec300d1173ba8e
jeighkoub <92068755+jeighkoub@users.noreply.github.com>
2025-05-15 20:20:22 -0400

    space bar

--- dropper.gd
+++ dropper.gd
@@ -9,3 +9,10 @@ extends Node2D
 
 func create_scoop(color: String) -> void:
 	pass
+
+func drop():
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("space_bar"):
+		drop();
