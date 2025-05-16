77ef9f3f4e22275deee2cd8273763f326ff5638c
jeighkoub <92068755+jeighkoub@users.noreply.github.com>
2025-05-15 20:05:07 -0400

    fix the shit merging deleted

---
+++ dropper.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var scoop: Node2D #Scoop
+
+@export var blue: Resource
+@export var green: Resource
+@export var red: Resource
+
+
+func create_scoop(color: String) -> void:
+	pass
