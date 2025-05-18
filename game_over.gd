3d8bb7779dd79b0977a4c72c511aed922fa8963d
DIGIT-DILL <charleshowarddill@gmail.com>
2025-05-18 11:55:59 -0400

    EDITS

--- game_over.gd
+++ game_over.gd
@@ -5,4 +5,6 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("ui_accept"):
 			$AudioStreamPlayer2.play()
+			set_process_input(false)
+			await $AudioStreamPlayer2.finished
 			get_tree().change_scene_to_file("res://index.tscn")
--- index.gd
+++ index.gd
@@ -2,7 +2,9 @@ extends Control
 
 
 func _input(event: InputEvent) -> void:
-		if event.is_action_pressed("ui_accept"):
-			$AudioStreamPlayer.play()
-			get_tree().change_scene_to_file("res://assets/cutscene_scenes/opening_cutscene.tscn")
+	if event.is_action_pressed("ui_accept"):
+		$AudioStreamPlayer.play()
+		set_process_input(false)
+		await $AudioStreamPlayer.finished
+		get_tree().change_scene_to_file("res://assets/cutscene_scenes/opening_cutscene.tscn")
  
